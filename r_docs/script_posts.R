e7b947405d46d873dca031657a3eb550dd655053
carina hoerst <carina.h1408@googlemail.com>
2021-03-04 20:51:03 +0000

    add in ndjson files, extract 2021-01-06, transform into csv

--- r_docs/script_posts.R
+++ r_docs/script_posts.R
@@ -1,7 +1,14 @@
 library(tidytext)
 library(tidyverse)
+library(ndjson)
 
 posts <- read_csv("./data/Parler_posts.csv")
+big_posts00 <- stream_in("./data/parler_data000000000000.ndjson")
+big_posts01 <- stream_in("./data/parler_data000000000001.ndjson")
+big_posts02 <- stream_in("./data/parler_data000000000002.ndjson")
+big_posts08 <- stream_in("./data/parler_data000000000008.ndjson")
+
+## posts data set
 
 # reduce dataset to only those entries that were posted "4 days ago" which was dated by the researcher to the 6th of January
 posts <- posts %>%
@@ -18,4 +25,30 @@ one_perc <- one_perc %>%
 # remove doubled entries
 one_perc <- one_perc %>%
   dplyr::filter(!duplicated(post_text))
+
+# transform into csv
+write.csv(one_perc, "post_1%.csv")
+
+
+## big posts data set
+
+big_posts_00 <- big_posts_00 %>%
+  dplyr::filter(str_detect(createdAtformatted, "2021-01-06"))
+
+big_posts <- big_posts %>%
+  dplyr::filter(str_detect(createdAtformatted, "2021-01-06"))
+
+big_posts01 <- big_posts01 %>%
+  dplyr::filter(str_detect(createdAtformatted, "2021-01-06"))
+
+big_posts02 <- big_posts02 %>%
+  dplyr::filter(str_detect(createdAtformatted, "2021-01-06"))
+
+# transform into csv
+write.csv(big_posts, "parler_data000000000008_20210106.csv")
+
+write.csv(big_posts01, "parler_data000000000001_20210106.csv")
+
+write.csv(big_posts02, "parler_data000000000002_20210106.csv")
+
 
