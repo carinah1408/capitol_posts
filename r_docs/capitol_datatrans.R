53c57eab25933d576ec33863f8e177bc0b38d45b
carina hoerst <carina.h1408@googlemail.com>
2021-03-16 16:31:04 +0000

    read in all "big_posts" files

--- r_docs/capitol_datatrans.R
+++ r_docs/capitol_datatrans.R
@@ -3,33 +3,10 @@ library(ndjson)
 library(plyr)
 
 ## datasets
+
+# recent Parler scrape
 posts0.1 <- read_csv("./data/Parler_posts.csv") # dataset taken from https://github.com/sbooeshaghi/parlertrick/blob/main/data/all_posts.csv.gz
-posts0.2 <- read_csv("./data/out.csv") # dataset scraped from html files (data_no_video.tar.gz)
-
-# already prepared datasets
-posts <- read_csv("./data/posts.csv") 
-
-big_meta00 <- read_csv("./data/big_meta00.csv")
-big_meta01 <- read_csv("./data/big_meta01.csv")
-
-big_posts05 <- read_csv("./data/big_posts05.csv")
-big_posts06 <- read_csv("./data/big_posts06.csv")
-
-
-data01 <- read_csv("./data/data01.csv")
-data02 <- read_csv("./data/data02.csv")
-data03 <- read_csv("./data/data03.csv")
-data04 <- read_csv("./data/data04.csv")
-data05 <- read_csv("./data/data05.csv")
-data06 <- read_csv("./data/data06.csv")
-data07 <- read_csv("./data/data07.csv")
-
-
-
-
-data15 <- read_csv("./data/data15.csv")
-data16 <- read_csv("./data/data16.csv")
-data17 <- read_csv("./data/data17.csv")
+posts0.2 <- read_csv("./data/out.csv") # newly scraped dataset from html files (data_no_video.tar.gz)
 
 # datasets from https://zenodo.org/record/4442460#.YEzn7J37SMp
 big_posts00 <- stream_in("./data/parler_data000000000000.ndjson") # no similarities were established with this dataset
@@ -37,7 +14,6 @@ big_posts01 <- stream_in("./data/parler_data000000000001.ndjson")
 big_posts02 <- stream_in("./data/parler_data000000000002.ndjson")
 big_posts03 <- stream_in("./data/parler_data000000000003.ndjson")
 big_posts04 <- stream_in("./data/parler_data000000000004.ndjson")
-
 big_posts05 <- stream_in("./data/parler_data000000000005.ndjson")
 big_posts06 <- stream_in("./data/parler_data000000000006.ndjson")
 big_posts07 <- stream_in("./data/parler_data000000000007.ndjson")
@@ -54,8 +30,37 @@ big_meta06 <- stream_in("./data/parler_user000000000006.ndjson")
 big_meta07 <- stream_in("./data/parler_user000000000007.ndjson")
 big_meta08 <- stream_in("./data/parler_user000000000008.ndjson")
 
+# already prepared datasets (see below) for further merging
+posts <- read_csv("./data/posts.csv") 
 
-## big posts data set
+big_meta00 <- read_csv("./data/big_meta00.csv")
+big_meta01 <- read_csv("./data/big_meta01.csv")
+
+big_posts01 <- read_csv("./data/big_posts01.csv")
+big_posts02 <- read_csv("./data/big_posts02.csv")
+big_posts03 <- read_csv("./data/big_posts03.csv")
+big_posts04 <- read_csv("./data/big_posts04.csv")
+big_posts05 <- read_csv("./data/big_posts05.csv")
+big_posts06 <- read_csv("./data/big_posts06.csv")
+big_posts07 <- read_csv("./data/big_posts07.csv")
+
+data01 <- read_csv("./data/data01.csv")
+data02 <- read_csv("./data/data02.csv")
+data03 <- read_csv("./data/data03.csv")
+data04 <- read_csv("./data/data04.csv")
+data05 <- read_csv("./data/data05.csv")
+data06 <- read_csv("./data/data06.csv")
+data07 <- read_csv("./data/data07.csv")
+data11 <- read_csv("./data/data11.csv")
+data12 <- read_csv("./data/data12.csv")
+data13 <- read_csv("./data/data13.csv")
+data14 <- read_csv("./data/data14.csv")
+data15 <- read_csv("./data/data15.csv")
+data16 <- read_csv("./data/data16.csv")
+data17 <- read_csv("./data/data17.csv")
+
+
+## "big posts" data set
 
 # select relevant columns
 big_meta00 <- big_meta00 %>%
@@ -89,7 +94,7 @@ big_meta08 <- big_meta08 %>%
 write.csv(big_meta01, "big_meta01.csv")
 
 
-# reduce datasets to only those entries that were posted on 6th of January
+# reduce datasets (only  entries from 6th of January)
 big_posts00 <- big_posts00 %>%
   dplyr::filter(str_detect(createdAtformatted, "2021-01-06"))
 
@@ -135,23 +140,21 @@ big_posts07 <- big_posts07 %>%
 
 write.csv(big_posts07, "big_posts07.csv")
 
-# merge datasets based on common username and remove "human == FALSE"
+# merge datasets based on common username
 
-data17 <- inner_join(big_posts07, big_meta01) 
+data14 <- inner_join(big_posts04, big_meta01) 
 
 # transform into csv
-write.csv(data17, "data17.csv")
+write.csv(data14, "data14.csv")
 
 
+## "posts" data set
 
-
-## posts data set
-
-# reduce dataset to only those entries that were posted "4 days ago" which was dated by the researcher to the 6th of January
+# reduce dataset (only entries posted "4 days ago" and "5 days ago" = dated by the researcher to the 6th of January)
 posts <- posts %>%
   dplyr::filter(post_timestamp >= "4 days ago" & post_timestamp <= "5 days ago") 
 
-# remove NAs cases
+# remove NAs 
 posts <- posts %>%
   dplyr::mutate(post_text = as.character(post_text)) %>%
   dplyr::filter(!is.na(post_text))
@@ -162,14 +165,14 @@ posts$author_username_posts_text <- paste(posts$author_username, posts$post_text
 # remove duplicates in new column
 posts <- ddply(posts,.(author_username_posts_text), head, n = 1)
 
-# select only those posts that contain "#"
+# select posts that contain "#"
 posts <- posts %>%
   dplyr::filter(grepl("#", post_text)) # results in 8,564 obs
 
 # export csv
 write.csv(posts, "posts.csv")
 
-# randomly selected 5% of the data set for pre-analysis
+# randomly select 5% of the data set for pre-analysis
 
 five_perc <- posts2[sample(1:nrow(posts2), 428), ]
 
@@ -177,10 +180,7 @@ five_perc <- posts2[sample(1:nrow(posts2), 428), ]
 write.csv(five_perc, "posts_5%.csv")
 
 
-## investigate data sets for similarities in usernames (bots)
-
-# read in data from edited datasets
-data01 <- read_csv("data01.csv")
+## investigate data sets for similarities in usernames and post ID (bots)
 
 # remove @ in five_perc author_username
 posts2$author_username = as.character(gsub("\\@", "", posts2$author_username))
